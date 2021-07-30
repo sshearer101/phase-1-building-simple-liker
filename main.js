c32003260c7c87b7dc637f6dc51f9268453c902d
Sam Shearer <sshearer101@gmail.com>
2021-07-30 17:46:09 -0600

    commit tree 4a42ec6e54df49551dcd09e5f1043600b8e79b62

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
-
+EMPTY_HEART.addeventlistener('click', () => {
+  
+})
 
 
 //------------------------------------------------------------------------------
